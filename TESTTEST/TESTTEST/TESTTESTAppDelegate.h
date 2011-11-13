59696bdcc891781866dceaa7a5584e7aeb9a9d0c
Your Name <timothy.urness@drake.edu>
2011-11-13 09:25:23 -0600

    initilize the whole thing

---
+++ TESTTEST/TESTTEST/TESTTESTAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  TESTTESTAppDelegate.h
+//  TESTTEST
+//
+//  Created by Timothy Urness on 11/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TESTTESTViewController;
+
+@interface TESTTESTAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet TESTTESTViewController *viewController;
+
+@end
---
+++ TESTTEST/TESTTEST/TESTTESTViewController.h
@@ -0,0 +1,15 @@
+//
+//  TESTTESTViewController.h
+//  TESTTEST
+//
+//  Created by Timothy Urness on 11/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TESTTESTViewController : UIViewController {
+    
+}
+
+@end
